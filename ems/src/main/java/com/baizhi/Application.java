78f22d26ae6f90ae2248985505d31dadec7634fc
luoyl3104 <49857091+luoyl3104@users.noreply.github.com>
2019-04-22 20:48:29 +0800

    修改入口类

--- ems/src/main/java/com/baizhi/Application.java
+++ ems/src/main/java/com/baizhi/Application.java
@@ -9,7 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
